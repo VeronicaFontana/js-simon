033349390d412abf6be5b3996e28e864e0648cea
VeronicaFontana <fontana.veronica98@gmail.com>
2023-09-20 22:20:55 +0200

    [ADD] Result logic

--- js/script.js
+++ js/script.js
@@ -2,32 +2,44 @@ const container = document.querySelector(".container");
 const numberBar = document.querySelector(".number-bar");
 const timerBar = document.querySelector(".timer-bar");
 const timer = document.querySelector(".timer");
-const restart = document.querySelector("button");
+const startBtn = document.querySelector("#btn-start");
+const restartBtn = document.querySelector("#btn-restart");
 const result = document.querySelector(".result");
+const resultBar = document.querySelector(".result-bar");
 const esito = document.querySelector(".esito");
-const numArr = [];
-const userArr = [];
+let numArr = [];
+let userArr = [];
+let errorArr = [];
+let correctArr = [];
+let messaggio = document.querySelector(".messaggio");
 let counterSecondi = 6;
 
 
-for(i = 0; i < 5; i++){
-  const number = randomizer(1, 10);
-  const span = document.createElement("span");
-  span.innerHTML = number;
-  numArr.push(number);
-  console.log(numArr);
-  numberBar.append(span);
+startBtn.addEventListener("click", function(){
+  start();
+  numberBar.classList.remove("d-none");
+  timerBar.classList.remove("d-none");
+})
+
+
+//FUNCTIONS
+function start(){
+  randomNumber()
+  startCountdown();
+
+  console.log("start eseguito")
 }
 
-startCountdown();
-
-
-
-
-
-
-
-
+function randomNumber(){
+  for(i = 0; i < 5; i++){
+    const number = randomizer(1, 10);
+    const span = document.createElement("span");
+    span.innerHTML = number;
+    numArr.push(number);
+    console.log(numArr);
+    numberBar.append(span);
+  }
+}
 
 function randomizer(min, max){
   const random = Math.floor(Math.random() * (max - min + 1) + min);
@@ -44,7 +56,8 @@ function startCountdown(){
 		}else{
       numberBar.classList.add("d-none");
       delayPrompt();
-      esito.classList.remove("d-none");
+      resultBar.classList.remove("d-none");
+      timerBar.classList.add("d-none");
 		}
 	},1000)
 }
@@ -61,6 +74,7 @@ function delayPrompt(){
       console.log(userArr);
     }
     clearInterval(interval);
+    confronto();
   }, 1000);
 } 
 
@@ -68,4 +82,48 @@ function promptFunction(){
   return prompt();
 }
 
+function confronto(){
+  let error = 0;
+  let correct = 0;
+  
 
+  for(i = 0; i < 5; i++){
+    if(numArr[i] == userArr[i]){
+      correct++;
+      correctArr.push(userArr[i])
+    }else if(numArr[i] != userArr[i]){
+      error++;
+      errorArr.push(userArr[i])
+    }
+  }
+
+  console.log(correctArr);
+  console.log(errorArr);
+
+  if(correctArr.length === 5){
+    messaggio.innerHTML = `Bravo! Hai indovinato tutti i numeri!`
+    restartBtn.classList.remove("d-none");
+  }else if(correctArr.length === 0){
+    messaggio.innerHTML = `Hai sbagliato tutti i numeri. Riprova!`
+    restartBtn.classList.remove("d-none");
+  }else{
+    messaggio.innerHTML = `Hai indovinato ${correct} numeri: ${correctArr}. Hai sbagliato ${error} numeri: ${errorArr}.`
+    restartBtn.classList.remove("d-none");
+  }
+}
+  
+
+  restartBtn.addEventListener("click", function(){
+    numberBar.classList.add("d-none");
+    resultBar.classList.add("d-none");
+    timerBar.classList.add("d-none");
+    restartBtn.classList.add("d-none");
+
+    numArr = [];
+    userArr = [];
+    correctArr = [];
+    errorArr = [];
+    counterSecondi = 6;
+    console.log("restart eseguito")
+    console.log([numArr, userArr, correctArr, errorArr])
+  })
